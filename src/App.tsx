1211cba58eedd97373fc32347f9e618c4e4db569
matheusifz <matheusifz@gmail.com>
2021-08-25 18:33:27 -0300

    feat: fetch data from ViaCep

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import Input from "./components/input";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [adress, setAdress] = useState({}); //Mostrar os dados
+  const [search, setSearch] = useState(""); // Procurar os dados
+  const [query, setQuery] = useState(""); // Informar os dados que eu quero pegar baseado na pesquisa feita
+
+  useEffect(() => {
+    getAdress();
+  }, [query]);
+
+  const getAdress = async () => {
+    const response = await fetch(`https://viacep.com.br/ws/01001000/json/`)
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  };
+
+
+
   return (
     <div className="App">
       <form className="search-form">
