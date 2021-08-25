3b3911fa254dfb17c0fce0c0960034025701cbb1
matheusifz <matheusifz@gmail.com>
2021-08-25 17:37:46 -0300

    feat: create input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Input from "./components/input";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form className="search-form">
+        <Input />
+      </form>
+    </div>
+  );
 }
 
 export default App;
