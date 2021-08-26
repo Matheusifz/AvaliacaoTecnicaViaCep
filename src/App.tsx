f806e8dc45490c3b27bf0f3466785743131f2953
matheusifz <matheusifz@gmail.com>
2021-08-26 16:52:58 -0300

    feat: display fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Input from "./components/input";
 import { useState } from "react";
 
+interface Address {
+  localidade: string;
+  bairro: string;
+  uf: string;
+  logradouro: string;
+  cep: string;
+}
+
 function App() {
-  const [address, setAddress] = useState({});
+  const [address, setAddress] = useState<Address | null>();
   const [query, setQuery] = useState("");
 
   const onChange = (e: any) => {
@@ -27,6 +35,15 @@ function App() {
           Search
         </button>
       </form>
+      {!!address && (
+        <div>
+          <h3>{address.localidade}</h3>
+          <h3>{address.logradouro}</h3>
+          <h3>{address.bairro}</h3>
+          <h3>{address.uf}</h3>
+          <h3>{address.cep}</h3>
+        </div>
+      )}
     </div>
   );
 }
