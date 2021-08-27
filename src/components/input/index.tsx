f35210ee58537f28af0da7cfbd51329be9eba598
matheusifz <matheusifz@gmail.com>
2021-08-27 20:24:19 -0300

    feat: style input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,10 +1,12 @@
+import { Input as StyledInput } from "./styles";
+
 interface InputProps {
   query: string;
   onChange: (event: any) => void;
 }
 
 const Input: React.FC<InputProps> = ({ query, onChange }) => {
-  return <input value={query} onChange={onChange}></input>;
+  return <StyledInput  value={query} onChange={onChange}></StyledInput>;
 };
 
 export default Input;
---
+++ src/components/input/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  background: Transparent no-repeat;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  border: none;
+  overflow: hidden;
+  outline: none;
+
+  width: 300px;
+  padding: 0 20px;
+  color: #fff;
+`;
